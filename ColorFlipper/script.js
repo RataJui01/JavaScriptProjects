47c743af082aeb1042d9b0daf867cb8f8ee7e0b8
Jui <itmonero@gmail.com>
2024-09-14 10:51:06 +0700

    Adding Eventlistener

--- ColorFlipper/script.js
+++ ColorFlipper/script.js
@@ -1,4 +1,19 @@
 const colorLabel = document.querySelector('span');
-const bgColor = document.querySelector('body');
+const bodyBgColor = document.querySelector('body');
+const button = document.querySelector('button');
 
-let bgColorlist = [];
+let bgColorlist = [
+    '#43e5a0','#b8fadd',
+    '#546fff','#b2c9ff',
+    '#fe6e78', '#ffe3e3',
+    '#ffa3fa', '#81f495',
+    '#ffe64b', '#ef9f48'
+];
+
+button.addEventListener('click', () => {
+    var randomIndex = [Math.floor(Math.random() * bgColorlist.length)];
+    var randomColor = bgColorlist[randomIndex];
+    bodyBgColor.style.backgroundColor = `${randomColor}`;
+    colorLabel.innerText = `${randomColor}`;
+    colorLabel.style.color = `${randomColor}`;
+})
